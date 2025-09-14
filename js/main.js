afe15080f28d49ad8f32e2711a333426cb232c6e
Sekwaticode <122540547+Sekwaticode@users.noreply.github.com>
2025-09-15 00:31:52 +0200

    some changes

--- js/main.js
+++ js/main.js
@@ -158,3 +158,26 @@ document.addEventListener("DOMContentLoaded", function() {
 
     timelineContainers.forEach(tc => observer.observe(tc));
 });
+
+// FAQs
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+  const btn = item.querySelector(".faq-question");
+
+  btn.addEventListener("click", () => {
+    // close other items
+    faqItems.forEach(i => {
+      if (i !== item) {
+        i.classList.remove("active");
+        i.querySelector(".icon").textContent = "+";
+      }
+    });
+
+    // toggle current item
+    item.classList.toggle("active");
+
+    const icon = item.querySelector(".icon");
+    icon.textContent = item.classList.contains("active") ? "–" : "+";
+  });
+});
